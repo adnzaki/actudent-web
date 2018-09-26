5318e1e14954486c6e9b9d53ee0e0e3889601d4d
Adnan Zaki <adnan_zaki@hotmail.co.id>
2018-09-26 11:35:58 +0700

    Memperbarui menu aplikasi

--- application/views/component/menu.php
+++ application/views/component/menu.php
@@ -5,7 +5,30 @@
         <a href="{admin}home"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Dashboard</span></a>
       </li>
       <li class="<?= menu_active('siswa', 'active') ?> nav-item">
-        <a href="{admin}siswa"><i class="la la-group"></i><span class="menu-title" data-i18n="nav.dash.main">Siswa</span></a>
+        <a href="{admin}siswa"><i class="la la-group"></i><span class="menu-title" data-i18n="nav.dash.main">Data Siswa</span></a>
+      </li>
+      <li class="<?= menu_active('siswa', 'active') ?> nav-item">
+        <a href="{admin}siswa"><i class="la la-check-square"></i><span class="menu-title" data-i18n="nav.dash.main">Kehadiran</span></a>
+      </li>
+      <li class="<?= menu_active('siswa', 'active') ?> nav-item">
+        <a href="{admin}siswa"><i class="la la-calendar-o"></i><span class="menu-title" data-i18n="nav.dash.main">Agenda Sekolah</span></a>
+      </li>
+      <li class="<?= menu_active('siswa', 'active') ?> nav-item">
+        <a href="{admin}siswa"><i class="la la-book"></i><span class="menu-title" data-i18n="nav.dash.main">Jadwal Pelajaran</span></a>
+      </li>
+      <li class="<?= menu_active('siswa', 'active') ?> nav-item">
+        <a href="{admin}siswa"><i class="la la-comments"></i><span class="menu-title" data-i18n="nav.dash.main">Pesan</span></a>
+      </li>
+      <li class="<?= menu_active('siswa', 'active') ?> nav-item">
+        <a href="{admin}siswa"><i class="la la-clipboard"></i><span class="menu-title" data-i18n="nav.dash.main">Nilai</span></a>
+      </li>
+      <li class=" nav-item"><a href="#"><i class="la la-gears"></i><span class="menu-title" data-i18n="nav.icons.main">Pengaturan</span></a>
+        <ul class="menu-content">
+          <li class="<?= menu_active('siswa', 'active') ?>"><a class="menu-item" href="#" data-i18n="nav.icons.icons_feather">Pengguna</a>
+          </li>
+          <li class="<?= menu_active('siswa', 'active') ?>"><a class="menu-item" href="#" data-i18n="nav.icons.icons_line_awesome">Aplikasi</a>
+          </li>
+        </ul>
       </li>
       
     </ul>
