5aa67f3e5182ea1b1af88ced4bd5823ac5decf03
Adnan Zaki <adnan_zaki@hotmail.co.id>
2018-10-11 23:06:08 +0700

    Menambahkan dokumentasi

--- application/core/ACController.php
+++ application/core/ACController.php
@@ -1,14 +1,49 @@
 <?php
+/**
+ * ACTUDENT
+ * 
+ * Attitude Control for Student
+ * 
+ * Software ini dilisensikan di bawah Wolestech Private License
+ * Copyright (c) 2018, Wolestech | Software Developer and Network Builder
+ * 
+ * Actudent adalah aplikasi yang diperuntukkan untuk sekolah sebagai sarana 
+ * "controlling" dan "monitoring" siswa di sekolah yang mencakup kehadiran siswa,
+ * kegiatan sekolah, jadwal pelajaran, nilai siswa serta sarana komunikasi antara sekolah dan orang tua
+ * 
+ * @copyright   Copyright (c) 2018, Wolestech
+ * @license     Wolestech Private License
+ * @author      WolesDev Team
+ * @link        https://wolestech.com
+ * @version     1.0.0 
+ */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * ACController Class
+ * Ini adalah class inti dari Actudent App. Class ini menyimpan fungsi umum dan 
+ * menjadi kunci agar aplikasi dapat berjalan.
+ */
 class ACController extends CI_Controller
 {
+
+    /**
+     * Class Constructor 
+     * 
+     * @return void
+     */
     public function __construct()
     {
         parent:: __construct();
         $this->load->model(['admin/SekolahModel' => 'sekolah']);
     }
 
+    /**
+     * Fungsi yang menyuplai variabel global untuk aplikasi 
+     * 
+     * @return array
+     */
     protected function shared()
     {
         $data = [
@@ -24,7 +59,13 @@ class ACController extends CI_Controller
         return $data;
     }
     
-    public function getDataSekolah($schoolID)
+    /**
+     * Mengambil data sekolah dari SekolahModel
+     * 
+     * @param int $schoolID
+     * @return object
+     */
+    protected function getDataSekolah($schoolID)
     {
         return $this->sekolah->getDataSekolah($schoolID)[0];
     }
--- application/models/admin/SekolahModel.php
+++ application/models/admin/SekolahModel.php
@@ -3,11 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SekolahModel extends CI_Model
 {
+    /**
+     * Tabel yang dibutuhkan untuk class AuthModel
+     * 
+     * @var array
+     */
     private $table = [
         'sekolah' => 'tb_school'
     ];
 
-    public function getDataSekolah($schoolID)
+    /**
+     * Mengambil data sekolah berdasarkan ID Sekolah 
+     * 
+     * @param int $schoolID
+     * @return object
+     */
+    public function getDataSekolah(int $schoolID)
     {
         return $this->db->get_where($this->table['sekolah'], ['schoolID' => $schoolID])->result();        
     }
