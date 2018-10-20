c547d5501795a601323f14ab1abe5da23bca52c0
Adnan Zaki <adnan_zaki@hotmail.co.id>
2018-10-20 20:49:37 +0700

    Menambahkan info baris per halaman

--- application/views/admin/pages/siswa/content.php
+++ application/views/admin/pages/siswa/content.php
@@ -67,35 +67,42 @@
                     </tbody>
                     </table>
                 </div>
-                <div class="col-lg-6 offset-lg-6 col-md-12">
-                    <div class="text-center mb-3">
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination justify-content-center pagination-round">
-                                <li v-bind:class="[linkClass]" @click="getSiswa(first, search)">
-                                    <a class="page-link" href="#" aria-label="Previous">
-                                        <span aria-hidden="true">&laquo;</span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                </li>
-                                <li v-bind:class="[linkClass]" @click="getSiswa(prev, search)">
-                                    <a class="page-link" href="#">Prev</a>
-                                </li>
-                                <li v-for="link in pageLinks" v-bind:class="[linkClass, activeLink(link)]" 
-                                    v-if="numLinks" @click="getSiswa((link - 1), search)">
-                                    <a class="page-link" href="#">{{ link }}</a>
-                                </li>
-                                <li v-bind:class="[linkClass]" @click="getSiswa(next, search)">
-                                    <a class="page-link" href="#">Next</a>
-                                </li>
-                                <li v-bind:class="[linkClass]" @click="getSiswa(last, search)">
-                                    <a class="page-link" href="#" aria-label="Next">
-                                        <span aria-hidden="true">&raquo;</span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="text-left pl-2 mb-3">
+                            <br><p>{{ rowRange }}</p>
+                        </div>
                     </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="text-center mb-3">
+                            <nav aria-label="Page navigation">
+                                <ul class="pagination justify-content-center pagination-round">
+                                    <li v-bind:class="[linkClass]" @click="getSiswa(first, search)">
+                                        <a class="page-link" href="#" aria-label="Previous">
+                                            <span aria-hidden="true">&laquo;</span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                    </li>
+                                    <li v-bind:class="[linkClass]" @click="getSiswa(prev, search)">
+                                        <a class="page-link" href="#">Prev</a>
+                                    </li>
+                                    <li v-for="link in pageLinks" v-bind:class="[linkClass, activeLink(link)]" 
+                                        v-if="numLinks" @click="getSiswa((link - 1), search)">
+                                        <a class="page-link" href="#">{{ link }}</a>
+                                    </li>
+                                    <li v-bind:class="[linkClass]" @click="getSiswa(next, search)">
+                                        <a class="page-link" href="#">Next</a>
+                                    </li>
+                                    <li v-bind:class="[linkClass]" @click="getSiswa(last, search)">
+                                        <a class="page-link" href="#" aria-label="Next">
+                                            <span aria-hidden="true">&raquo;</span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>                
                 </div>
             </div>
         </div>
