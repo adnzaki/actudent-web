05e116b57a599f6428fe57bbbd3e8ddbeaa7a693
mukliz <mukliz>
2019-02-02 22:13:17 +0700

    Update modul kelas

--- application/views/admin/pages/kelas/data-kelas.php
+++ application/views/admin/pages/kelas/data-kelas.php
@@ -1,27 +1,33 @@
 <div class="card-content collapse show">
     <div class="card-body">
-        <div class="row">
-            <div class="col-6">
+        <div class="row">   
+            <div class="col-sm-12">
+                <flash-alert :alert-class="flashAlert.class" :title="flashAlert.title" :icon="flashAlert.icon" 
+                :text="flashAlert.text" v-if="flashAlert.show" />
+            </div>
+        </div>
+        <div class="row">         
+            <div class="col-12 col-md-4 col-lg-6 col-xl-7">
                 <div class="form-group">
-                    <button @click="showFormTambah" type="button" class="btn mr-1 mb-1 btn-primary btn-sm" data-toggle="modal" data-target="#iconModal">
-                        <i class="la la-plus"></i> Tambah
-                    </button>
-                    <button type="button" class="btn mr-1 mb-1 btn-danger btn-sm"><i class="la la-trash"></i> Hapus</button>
-                    <button type="button" class="btn mr-1 mb-1 btn-light btn-sm"><i class="la la-filter"></i> Filter</button>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-primary box-shadow-1" 
+                            data-toggle="modal" data-target="#iconModal">Tambah 
+                        </button>
+                        <button type="button" class="btn btn-danger box-shadow-1">Hapus</button> 
+                        <button type="button" class="btn btn-light box-shadow-1">Filter</button>
+                    </div>
                 </div>
             </div>
-            <div class="col-3">
-                <fieldset class="form-group">
-                    <select class="form-control" v-model="rows" id="basicSelect" v-on:change="showPerPage">
-                        <option value="10">10 baris</option>
-                        <option value="25">25 baris</option>
-                        <option value="50">50 baris</option>
-                        <option value="100">100 baris</option>
-                        <option value="250">250 baris</option>
-                    </select>
-                </fieldset>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <select class="select2 form-control" id="showRows" style="width: 100%;">
+                    <option value="10">10 baris</option>
+                    <option value="25">25 baris</option>
+                    <option value="50">50 baris</option>
+                    <option value="100">100 baris</option>
+                    <option value="250">250 baris</option>
+                </select>
             </div>
-            <div class="col-3">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                 <fieldset>
                     <div class="input-group">
                         <input type="text" class="form-control" @keyup.enter="filter" v-model="search"
@@ -39,17 +45,17 @@
         <thead>
             <tr>
                 <th>#</th>
-                <th @click="sortData('studentNis')">NIS<i class="la la-sort"></th>
-                <th @click="sortData('studentName')">Nama Siswa<i class="la la-sort"></th>
-                <th @click="sortData('gradeName')">Kelas<i class="la la-sort"></th>
+                <th @click="sortData('grade_name')">Nama Kelas<i class="la la-sort"></th>
+                <th @click="sortData('periot_from')">Periot Start<i class="la la-sort"></th>
+                <th @click="sortData('periot_until')">Periot Last<i class="la la-sort"></th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="(item, index) in data" :key="index">
                 <td scope="row">{{ index + 1 }}</td>
-                <td>{{ item.studentNis }}</td>
-                <td>{{ item.studentName }}</td>
-                <td>{{ item.gradeName }}</td>
+                <td>{{ item.grade_name }}</td>
+                <td>{{ item.period_from }}</td>
+                <td>{{ item.period_until }}</td>
             </tr>
         </tbody>
         </table>
