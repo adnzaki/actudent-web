55bb9bd82cb92d7b63f15c40b89cd5096e8347f8
adnzaki <adnan_zaki@hotmail.co.id>
2019-02-05 23:33:29 +0700

    Merapikan user interface

--- application/views/admin/pages/kelas/data-kelas.php
+++ application/views/admin/pages/kelas/data-kelas.php
@@ -1,31 +1,33 @@
 <div class="card-content collapse show">
     <div class="card-body">
         <div class="row">
-            <div class="col-6">
+        <div class="col-12 col-md-4 col-lg-6 col-xl-7">
                 <div class="form-group">
-                    <button @click="showFormTambah" type="button" class="btn mr-1 mb-1 btn-primary btn-sm" data-toggle="modal" data-target="#iconModal">
-                        <i class="la la-plus"></i> Tambah
-                    </button>
-                    <button type="button" class="btn mr-1 mb-1 btn-danger btn-sm"><i class="la la-trash"></i> Hapus</button>
-                    <button type="button" class="btn mr-1 mb-1 btn-light btn-sm"><i class="la la-filter"></i> Filter</button>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button @click="showFormTambah" type="button" class="btn btn-primary box-shadow-1" 
+                            data-toggle="modal" data-target="#iconModal">{{ lang.tambah }}
+                        </button>
+                        <button type="button" class="btn btn-danger box-shadow-1">{{ lang.hapus }}</button> 
+                        <button type="button" class="btn btn-light box-shadow-1">Filter</button>
+                    </div>
                 </div>
             </div>
-            <div class="col-3">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
                 <fieldset class="form-group">
                     <select class="form-control" v-model="rows" id="basicSelect" v-on:change="showPerPage">
-                        <option value="10">10 baris</option>
-                        <option value="25">25 baris</option>
-                        <option value="50">50 baris</option>
-                        <option value="100">100 baris</option>
-                        <option value="250">250 baris</option>
+                    <option value="10">10 <?= lang('baris') ?></option>
+                    <option value="25">25 {{ lang.baris }}</option>
+                    <option value="50">50 {{ lang.baris }}</option>
+                    <option value="100">100 {{ lang.baris }}</option>
+                    <option value="250">250 {{ lang.baris }}</option>
                     </select>
                 </fieldset>
             </div>
-            <div class="col-3">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                 <fieldset>
                     <div class="input-group">
                         <input type="text" class="form-control" @keyup.enter="filter" v-model="search"
-                        placeholder="Cari nama, NIS, kelas" aria-describedby="button-addon2">
+                        :placeholder="lang.siswa_cari" aria-describedby="button-addon2">
                         <div class="input-group-append">
                             <button class="btn btn-primary" type="button" @click="filter">Go</button>
                         </div>
@@ -39,56 +41,29 @@
         <thead>
             <tr>
                 <th>#</th>
-                <th @click="sortData('studentNis')">NIS<i class="la la-sort"></th>
-                <th @click="sortData('studentName')">Nama Siswa<i class="la la-sort"></th>
-                <th @click="sortData('gradeName')">Kelas<i class="la la-sort"></th>
+                <th @click="sortData('student_nis')">NIS<i class="la la-sort"></th>
+                <th @click="sortData('student_name')">{{ lang.siswa_nama }}<i class="la la-sort"></th>
+                <th @click="sortData('grade_name')">{{ lang.siswa_kelas }}<i class="la la-sort"></th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="(item, index) in data" :key="index">
                 <td scope="row">{{ index + 1 }}</td>
-                <td>{{ item.studentNis }}</td>
-                <td>{{ item.studentName }}</td>
-                <td>{{ item.gradeName }}</td>
+                <td>{{ item.student_nis }}</td>
+                <td>{{ item.student_name }}</td>
+                <td>{{ item.grade_name }}</td>
             </tr>
         </tbody>
         </table>
     </div>
-    <div class="row">
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="text-left pl-2 mb-3">
-                <br><p>{{ rowRange }}</p>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="text-center mb-3">
-                <nav aria-label="Page navigation">
-                    <ul class="pagination justify-content-center pagination-round">
-                        <li v-bind:class="[linkClass]" @click="nav(first)">
-                            <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </li>
-                        <li v-bind:class="[linkClass]" @click="nav(prev)">
-                            <a class="page-link" href="#">Prev</a>
-                        </li>
-                        <li v-for="link in pageLinks" v-bind:class="[linkClass, activeLink(link)]" 
-                            v-if="numLinks" @click="nav((link - 1))">
-                            <a class="page-link" href="#">{{ link }}</a>
-                        </li>
-                        <li v-bind:class="[linkClass]" @click="nav(next)">
-                            <a class="page-link" href="#">Next</a>
-                        </li>
-                        <li v-bind:class="[linkClass]" @click="nav(last)">
-                            <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>                
-    </div>
+    <pager 
+        :link-class="linkClass"
+        :page-links="pageLinks"
+        :num-links="numLinks"
+        :active-link="activeLink"
+        :nav="nav" 
+        :first="first" :prev="prev"
+        :next="next" :last="last"
+        :row-range="rowRange"
+    />
 </div>
