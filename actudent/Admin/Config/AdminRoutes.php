7e96c8c62bda80e1c2a3e1e207cd7a320949cc0a
adnzaki <adnan_zaki@hotmail.co.id>
2019-03-22 12:51:20 +0700

    Memisahkan router sub-system ke modulnya sendiri

---
+++ actudent/Admin/Config/AdminRoutes.php
@@ -0,0 +1,25 @@
+<?php
+
+$routes->group('admin', ['namespace' => 'Actudent\Admin\Controllers'], function($routes)
+{
+	$routes->get('/', 'Home::goToHome');
+	$routes->add('login', 'Auth::index');
+	$routes->add('login/validate', 'Auth::validasi');
+	$routes->add('logout', 'Auth::logout');
+	$routes->add('home', 'Home::index');
+	$routes->add('siswa', 'Siswa::index');
+	$routes->add('siswa/get-kelas', 'Siswa::getKelas');
+	$routes->add('siswa/get-siswa/(:any)/(:any)/(:any)/(:any)/(:any)/(:any)', 'Siswa::getDataSiswa/$1/$2/$3/$4/$5/$6');
+	$routes->add('siswa/get-siswa/(:any)/(:any)/(:any)/(:any)/(:any)', 'Siswa::getDataSiswa/$1/$2/$3/$4/$5');
+	$routes->add('kelas', 'Kelas::index');
+	$routes->add('kelas/get-kelas/(:any)/(:any)/(:any)/(:any)/(:any)/(:any)', 'Kelas::getDataKelas/$1/$2/$3/$4/$5/$6');
+	$routes->add('kelas/get-kelas/(:any)/(:any)/(:any)/(:any)/(:any)', 'Kelas::getDataKelas/$1/$2/$3/$4/$5');
+	$routes->add('agenda', 'Agenda::index');
+	$routes->add('agenda/get-events', 'Agenda::getEvents');
+	$routes->add('pengaturan-aplikasi', 'Setting::index');
+	$routes->add('pengaturan-aplikasi/set-tema/(:any)', 'Setting::setWarnaTema/$1');
+	$routes->add('pengaturan-aplikasi/set-bahasa/(:any)', 'Setting::setBahasa/$1');
+	$routes->add('test-match', 'Test::testMatch');
+	$routes->add('hash', 'Test::hash');
+	$routes->add('test-login', 'Test::validateLogin');
+});
