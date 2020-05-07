4323cc5104b5504e112d7a2e797c8029083329dd
adnzaki <adnan_zaki@hotmail.co.id>
2020-05-07 13:27:32 +0700

    Fixed conflict

--- app/Language/english/AdminAbsensi.php
+++ app/Language/english/AdminAbsensi.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-<<<<<<< HEAD
     // teks halaman absensi
     'page_title'                    => 'Presence',
     'absensi_title'                 => 'Student Presence',
@@ -17,28 +16,4 @@ return [
     'absensi_sertakan_pr'           => 'Include homework',
     'absensi_label_pr'              => 'Homework',
     'absensi_detail_pr'             => 'Homework details',
-=======
-    // teks halaman data kelas
-    'page_title'                      => 'Present',
-    'absensi_title'                   => 'Present Data',
-    //'mapel_save_progress'           => 'Saving lessons data...',
-    // 'mapel_error_text'              => 'Can not save data, fill the form corectly please',
-    // 'mapel_insert_success'          => 'Successfully added lesson data',
-    // 'mapel_update_success'          => 'Lesson data has been updated successfully',
-    // 'mapel_delete_success'          => 'Lesson data has been deleted successfully',
-    // 'mapel_cari'                    => 'Search for Lessons code and lessons name',
-    // 'mapel_kode'                    => 'Lessons Code',
-    // 'mapel_nama'                    => 'Lessons Name',
-    // 'mapel_form_add_title'          => 'Add Lesson',    
-    // 'mapel_edit_title'              => 'Edit lesson data',
-    // 'mapel_kode'                    => 'Lesson code',
-    // 'mapel_nama'                    => 'Lesson name',
-    // 'mapel_input_kode'              => 'Input lesson name initial',
-    // 'mapel_input_nama'              => 'Input lesson name',
-    // 'mapel_err_code_required'       => 'Lesson Code is required',
-    // 'mapel_err_code_min'            => 'Lesson Code must have at least 3 characters',
-    // 'mapel_err_code_max'            => 'Lesson Code must only have up to 10 characters',
-    // 'mapel_err_code_duplicate'      => 'Lesson Code has been used',
-    // 'mapel_err_name_req'            => 'Lesson name is required',
->>>>>>> ec668cabd06fdf0a2ddd886e1841bda7f66905a2
 ];
