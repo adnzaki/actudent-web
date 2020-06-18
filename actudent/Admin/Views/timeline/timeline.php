7ce5ea90d2a76c3ddd84486937ea52536ca4433c
adnzaki <adnan_zaki@hotmail.co.id>
2020-06-18 12:00:47 +0700

    Removed timeline header row

--- actudent/Admin/Views/timeline/timeline.php
+++ actudent/Admin/Views/timeline/timeline.php
@@ -22,11 +22,6 @@ data-open="click" data-menu="vertical-menu" data-col="2-columns">
   <!-- ////////////////////////////////////////////////////////////////////////////-->
   <div class="app-content content">
     <div class="content-wrapper">
-      <div class="content-header row">
-        <div class="content-header-left col-md-6 col-12 mb-2 breadcrumb-new">
-          <h3 class="content-header-title mb-0 d-inline-block">{+ lang AdminTimeline.timeline_title +}</h3>
-        </div> 
-      </div>
       <div class="content-body" id="timeline-content">
         {+ include Actudent\Admin\Views\timeline\TimelineContent +}
       </div>
