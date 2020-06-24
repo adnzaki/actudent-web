ea3dec459fb614aba2e116cefc3d71f75fd29501
mukliz <mukliz>
2020-06-24 22:09:39 +0700

    Added feedback validation

--- app/Language/english/AdminFeedback.php
+++ app/Language/english/AdminFeedback.php
@@ -2,11 +2,17 @@
 
 return [
     // teks halaman umpan balik aplikasi
-    'page_title'            => 'Feedback',
-    'app_feedback_title'    => 'Application feedback',
-    'app_feedback_form'     => 'Feedback Form',
-    'app_feedback_type'     => 'Chose feedback type',
-    'feedback_label_desc'   => 'Feedback Description',
-    'feedback_input_desc'   => 'Descripe your feedback here..',
-    'feedback_label_att'    => 'Attachment',
+    'page_title'                => 'Feedback',
+    'app_feedback_title'        => 'Application feedback',
+    'app_feedback_form'         => 'Feedback Form',
+    'app_feedback_type'         => 'Chose feedback type',
+    'feedback_label_desc'       => 'Feedback Description',
+    'feedback_input_desc'       => 'Descripe your feedback here..',
+    'feedback_label_att'        => 'Attachment',
+    'feedback_send_progress'    => 'Sanding feedback...',
+    'feedback_error_text'       => 'Can not send feedback, fill the form corectly please',
+    'feedback_send_success'     => 'Feedback has been send successfully',
+    'feedback_err_type_req'     => 'Feedback type is required',
+    'feedback_err_desc_req'     => 'Feedback Description is required',
+    'feedback_err_desc_min'     => 'Feedback Description must have at least 20 characters',
 ];
