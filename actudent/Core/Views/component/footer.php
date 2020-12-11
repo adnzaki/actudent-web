3a1b6b816a37d5bf981eb006a2f9be5d16d73281
adnzaki <adnan_zaki@hotmail.co.id>
2020-12-11 11:35:45 +0700

    Changed footer to be fixed position

--- actudent/Core/Views/component/footer.php
+++ actudent/Core/Views/component/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer footer-static {footerColor} navbar-border navbar-shadow">
+<footer class="footer fixed-bottom {footerColor} navbar-border navbar-shadow">
   <p class="clearfix blue-grey {footerTextColor} text-sm-center mb-0 px-2">
     <span class="float-md-left d-block d-md-inline-block">Copyright &copy; 2020 <a class="text-bold-800 grey darken-2" href="https://wolestech.com"
       target="_blank">Wolestech</a>, All rights reserved. </span>
