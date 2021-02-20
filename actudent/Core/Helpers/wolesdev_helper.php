9902dc0c90a49dbafbb5176b5e9ef3813da78ff5
adnzaki <adnan_zaki@hotmail.co.id>
2021-02-20 22:10:42 +0700

    Added db_installed, html_text and access_denied

--- actudent/Core/Helpers/wolesdev_helper.php
+++ actudent/Core/Helpers/wolesdev_helper.php
@@ -15,6 +15,8 @@ if ( ! function_exists('parse'))
     /**
      * An alias function to parser renderer
      * 
+     * @author Adnan Zaki
+     * 
      * @param string $view
      * @param array $data
      * 
@@ -38,5 +40,116 @@ if ( ! function_exists('os_date'))
     function os_date()
     {
         return new \OstiumDate();
+    }
+}
+
+if ( ! function_exists('access_denied'))
+{
+    /**
+     * Tell users that the page they try to access is denied
+     * 
+     * @return string
+     */
+    function access_denied()
+    {
+        $style = [
+            'div' => [
+                'padding' => '5px',
+                'background-color' => 'yellow',
+                'border' => 'solid 3px',
+                'font-family' => 'Arial',
+                'text-align' => 'center',
+            ],
+        ];
+
+        $text = 'You do not have access to this page.';
+        return html_text($text, 'div > h2 > i', $style);
+    }
+}
+
+if ( ! function_exists('html_text'))
+{
+    /**
+     * A simple HTML element generator
+     * Outer HTML can be passed by using its tag name
+     * Example outer HTML creation: "p > i"
+     * will generate <p><i></i></p>
+     * 
+     * @author Adnan Zaki
+     * 
+     * @param string $inner
+     * @param string $outer
+     * @param array $style
+     * 
+     * @return string
+     */
+    function html_text(string $inner, string $outer = 'h2', array $style = [])
+    {    
+        function createStyle($outer, $style)    
+        {
+            $styleStr = '';
+            if(isset($style[$outer]))
+            {
+                $styleStr = 'style="';
+                $closeStyle = '';
+                $i = 1;
+                foreach($style[$outer] as $k => $v)
+                {                            
+                    if($i === count($style[$outer]))
+                    {
+                        $closeStyle = '"';
+                    }
+                    $styleStr .= $k .': ' . $v . ';' . $closeStyle;
+                    $i++;
+                }
+
+                return $styleStr;
+            }
+        }
+
+        if(strpos($outer, '>') === false)
+        {
+            $styleStr = createStyle($outer, $style);
+            return '<'.$outer. ' ' .$styleStr.'>'. $inner .'</'.$outer.'>';
+        }
+        else
+        {
+            $removeSpace = str_replace(' ', '', $outer);
+            $elems = explode('>', $removeSpace);
+            $outerOpen = '';
+            $outerClose = '';
+            $outerCloseWrapper = [];
+            foreach($elems as $val)
+            {
+                $styleStr = createStyle($val, $style);
+                $outerOpen .= '<' . $val . ' ' . $styleStr . '>';
+                $outerCloseWrapper[] = '</' . $val . '>';
+            }
+
+            $outerClose = implode('', array_reverse($outerCloseWrapper));
+
+            return $outerOpen . $inner . $outerClose;            
+        }
+    }
+}
+
+if ( ! function_exists('db_installed'))
+{
+    /**
+     * Check if Actudent's database has been installed properly
+     * 
+     * @return boolean
+     */
+    function db_installed()
+    {
+        $conn = new \Actudent\Core\Models\Connector;
+        if(count($conn->db->listTables()) >= 20)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
 }
