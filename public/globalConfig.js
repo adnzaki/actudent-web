6f01ae0b31a9f1cab0c052b25f8b3a17c4a1edf9
adnzaki <adnan_zaki@hotmail.co.id>
2021-03-25 13:22:33 +0700

    Added global configuration

---
+++ public/globalConfig.js
@@ -0,0 +1,9 @@
+const mode = 'development' // development, build, production
+
+const baseUrl = () => {
+    return (mode !== 'production')
+            ? `http://${window.location.hostname}/actudent/public/`
+            : `https://${window.location.hostname}/public/`
+}
+
+export { mode, baseUrl }
