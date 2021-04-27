ce68ee5a4e95faa39b788218908d4af3b7994c06
adnzaki <adnan_zaki@hotmail.co.id>
2021-04-27 09:29:52 +0700

    Try components splitting

--- public/ui/src/pages/parent/ParentMain.vue
+++ public/ui/src/pages/parent/ParentMain.vue
@@ -5,33 +5,7 @@
         <div class="text-h6">Our Changing Planet</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
-
-      <q-markup-table>
-        <thead>
-          <tr>
-            <th class="text-left">{{ lang.ortu_kk }}</th>
-            <th class="text-right">{{ lang.ortu_label_ayah }}</th>
-            <th class="text-right">{{ lang.ortu_label_ibu }}</th>
-            <th class="text-right">{{ lang.ortu_label_telp }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, index) in data">
-            <td class="text-left">{{ item.parent_family_card }}</td>
-            <td class="text-right">{{ item.parent_father_name }}</td>
-            <td class="text-right">{{ item.parent_mother_name }}</td>
-            <td class="text-right">{{ item.parent_phone_number }}</td>
-          </tr>
-        </tbody>
-      </q-markup-table>
-      <div class="q-pa-lg flex flex-center">
-        <q-pagination class="flex flex-center"
-          v-model="current"
-          :max="pageLinks.length"
-          input
-          @update:model-value="onPaginationUpdate"
-        />
-      </div>
+      <parent-table />
     </q-card>
   </div>
 </template>
@@ -39,17 +13,17 @@
 <script>
 import { defineComponent } from 'vue'
 import locale from '../../mixins/fetch-lang'
-import parent from '../../mixins/ac-ortu'
+import ParentTable from './ParentTable'
 
 export default defineComponent({
   name: 'ParentMain',
-  mixins: [locale, parent],  
+  mixins: [locale], 
+  components: { ParentTable },
   data () {
     return {}
   },
   mounted () {
     setTimeout(() => {
-      this.getOrtu()
       this.fetchLang('Admin')
       this.fetchLang('AdminOrtu')
     }, 500)
