1196a9301493257cd3ac7b60e22da347a3cce6b7
Adnan Zaki <adnan_zaki@hotmail.co.id>
2021-06-12 00:23:39 +0700

    More mobile focused on table field

--- app/ui-src/src/pages/parent/ParentTable.vue
+++ app/ui-src/src/pages/parent/ParentTable.vue
@@ -5,25 +5,38 @@
         <thead>
           <tr>
             <th class="text-left cursor-pointer"><q-checkbox v-model="$store.state.parent.checkAll" @update:model-value="selectAll" /></th>
-            <th class="text-left cursor-pointer" @click="sortData('parent_family_card')">{{ lang.ortu_kk }} <sort-icon /></th>
+            <th class="text-left cursor-pointer mobile-hide" @click="sortData('parent_family_card')">{{ lang.ortu_kk }} <sort-icon /></th>
             <th class="text-left cursor-pointer" @click="sortData('parent_father_name')">{{ lang.ortu_label_ayah }} <sort-icon /></th>
             <th class="text-left cursor-pointer" @click="sortData('parent_mother_name')">{{ lang.ortu_label_ibu }} <sort-icon /></th>
-            <th class="text-left">{{ lang.ortu_label_telp }}</th>
+            <th class="text-left mobile-hide">{{ lang.ortu_label_telp }}</th>
             <th class="text-left">{{ lang.aksi }}</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(item, index) in data" :key="index">
             <td class="text-left"><q-checkbox v-model="$store.state.parent.selectedParents" :val="`${item.parent_id}-${item.user_id}`" /></td>
-            <td class="text-left">{{ item.parent_family_card }}</td>
+            <td class="text-left mobile-hide">{{ item.parent_family_card }}</td>
             <td class="text-left">{{ item.parent_father_name }}</td>
             <td class="text-left">{{ item.parent_mother_name }}</td>
-            <td class="text-left">{{ item.parent_phone_number }}</td>
+            <td class="text-left mobile-hide">{{ item.parent_phone_number }}</td>
             <td class="text-left">
-              <q-btn-group>
+              <q-btn-group class="mobile-hide">
                 <q-btn color="accent" icon="edit" />
                 <q-btn color="accent" icon="delete" />
               </q-btn-group>
+              <q-btn round icon="more_vert" color="accent" class="mobile-only">
+                <q-menu>
+                  <q-list style="min-width: 100px">
+                    <q-item clickable v-close-popup>
+                      <q-item-section>{{ lang.perbarui }}</q-item-section>
+                    </q-item>
+                    <q-separator />
+                    <q-item clickable v-close-popup>
+                      <q-item-section>{{ lang.hapus }}</q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-btn>
             </td>
           </tr>
         </tbody>
