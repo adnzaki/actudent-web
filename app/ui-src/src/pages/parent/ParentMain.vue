cf5860f8e9be97fcef3940218058d971d5df8488
Adnan Zaki <adnan_zaki@hotmail.co.id>
2021-06-20 15:23:35 +0700

    Fix DeleteConfirm import

--- app/ui-src/src/pages/parent/ParentMain.vue
+++ app/ui-src/src/pages/parent/ParentMain.vue
@@ -26,7 +26,6 @@ import ParentTable from './ParentTable'
 import MainButton from './MainButton'
 import AddParentForm from './AddParentForm'
 import EditParentForm from './EditParentForm'
-import DeleteConfirm from 'components/DeleteConfirm'
 
 export default {
   name: 'ParentMain',
