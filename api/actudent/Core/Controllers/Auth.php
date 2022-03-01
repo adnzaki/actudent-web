ee73f9d0f380ea4b53a0b527f0b61bba75751098
Adnan Zaki <adnan_zaki@hotmail.co.id>
2022-03-02 01:44:05 +0700

    Remove unused code

--- api/actudent/Core/Controllers/Auth.php
+++ api/actudent/Core/Controllers/Auth.php
@@ -6,22 +6,6 @@ class Auth extends \Actudent
 {
     private $tokenExp = 12 * 30 * 24 * 60 * 60; // 1 year
 
-    public function index()
-    {
-        $cookie = get_cookie('remember_login');
-        $userToken = $this->auth->getUserToken($cookie);
-        if(session('email') !== null && session('userLevel') === '1' || $userToken !== false)
-        {
-            return redirect()->to(base_url('admin/home'));
-        }
-        else 
-        {            
-            $data = $this->common();
-            $data['title'] = lang('AdminAuth.login_title');
-            return parse('Actudent\Admin\Views\new-auth', $data);
-        }        
-    }
-
     public function isValidLogin()
     {
         $subscriber = new \Actudent\Core\Models\SubscriptionModel;
@@ -64,26 +48,5 @@ class Auth extends \Actudent
                 return $this->response->setJSON(['msg' => 'invalid']);
             }
         }
-    }
-
-    public function logout()
-    {
-        // save language option after user has been logged out from the app
-        $this->setLanguage($this->getUserLanguage());
-
-        // set network status to offline
-        $this->auth->statusJaringan('offline', $_SESSION['email']);
-
-        // delete token from database
-        $this->auth->deleteToken(get_cookie('remember_login'));
-
-        // remove session...
-        session()->remove(['id', 'email', 'nama', 'userLevel', 'logged_in']);
-
-        // delete cookie
-        delete_cookie('remember_login');
-
-        // redirect to login page
-        return redirect()->to(base_url('admin/login'));
     }
 }
