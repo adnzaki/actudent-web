a4e809af89dcab23ebfa3d92c751740075bd082b
Adnan Zaki <adnan_zaki@hotmail.co.id>
2022-04-05 14:42:39 +0700

    Fix incorrect userType data type

--- app/ui-src/src/store/presence/getters.js
+++ app/ui-src/src/store/presence/getters.js
@@ -3,7 +3,7 @@ import { date } from 'quasar'
 
 export default {
   presenceApi() {
-    return Cookies.get(conf.userType) === 1
+    return Cookies.get(conf.userType) === '1'
       ? `${conf.adminAPI}absensi/`
       : `${conf.teacherAPI}absensi/`
   },
