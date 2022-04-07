61fccc6b583654285163b370e81e17863f2134d8
Adnan Zaki <adnan_zaki@hotmail.co.id>
2022-04-08 00:18:14 +0700

    [Release] build ac.v2.0033

--- app/ui-src/src/pages/home/latest-updates.js
+++ app/ui-src/src/pages/home/latest-updates.js
@@ -4,13 +4,16 @@ const indonesia = [
     desc: `Komponen pencarian dropdown kini menggunakan metode asynchronous yang lebih cepat minim bug`
   },
   {
-    key: '[Perbaikan] Perbaikan bug kesalahan pada pengecekan hak ases pengguna (Admin atau Guru)',
-    desc: 'Perbaikan bug ini membuat pilihan jadwal pada fitur kehadiran siswa dapat bekerja kembali'
+    key: '[Peningkatan] Tabel rekapitulasi bulanan',
+    desc: 'Tampilan tabel rekapitulasi absen bulanan kini telah disesuaikan untuk pengguna mobile'
   },
   {
-    key: '[Perbaikan] Perbaikan bug tidak dapat menggunakan opsi default pada komponen dropdown',
-    desc: `Perbaikan ini membuat pengguna tidak perlu memilih salah satu opsi pada dropdown terlebih dahulu
-           untuk menyimpan data`
+    key: '[Perbaikan] Bug hak ases pengguna (Admin atau Guru)',
+    desc: 'Memperbaiki bug gagal memuat jadwal pelajaran pada kehadiran siswa'
+  },
+  {
+    key: '[Perbaikan] Bug komponen dropdown',
+    desc: `Memperbaiki tidak adanya nilai default komponen dropdown saat memasuki halaman baru`
   }
 ]
 
@@ -20,12 +23,16 @@ const english = [
     desc: `Dropdown search now uses asynchronous method that faster and less buggy`
   },
   {
-    key: '[Fix] Fix bug incorrect user access privilage (Admin or Teacher)',
-    desc: 'This fix makes schedule options on presence feature able to work again'
+    key: '[Improvement] Monthly summary table',
+    desc: 'Monthly summary table now has been suited for mobile users'
   },
   {
-    key: '[Fix] Fix bug unable to use default option on dropdown component',
-    desc: 'This fix makes users do not need to choose an option first on dropdown before submitting the form'
+    key: '[Fix] User access privilage bug (Admin or Teacher)',
+    desc: 'Fixed bug unable to load schedules on student presence'
+  },
+  {
+    key: '[Fix] Dropdown component bug',
+    desc: 'Fixed missing dropdown component default selected value when entering a new page'
   }
 ]
 
