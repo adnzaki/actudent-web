c0f08be63e8bf8501ae4fbe5d218e017247f2bce
Adnan Zaki <adnan_zaki@hotmail.co.id>
2022-05-12 10:46:26 +0700

    [Agenda] Fix default date range

--- app/ui-src/src/pages/agenda/Calendar.vue
+++ app/ui-src/src/pages/agenda/Calendar.vue
@@ -23,15 +23,16 @@ export default {
     const initialDate = ref(date.formatDate(new Date(), 'YYYY-MM-DD'))
     const fullCalendar = ref(null)
 
-    const defaultStart = date.startOfDate(new Date(), 'month')
-    const defaultEnd = date.addToDate(defaultStart, { months: 2 })
+    const defaultStart = date.startOfDate(new Date(), 'month')    
+    const defaultStartDate = date.subtractFromDate(defaultStart, { days: 7 })
+    const defaultEnd = date.addToDate(defaultStart, { days: 14, months: 1 })
 
     // formatting in one function
     const formatDate = v => date.formatDate(v, 'YYYY-MM-DD')
 
     store.dispatch('agenda/getEvents', {
       view: initialView.value,
-      start: formatDate(defaultStart),
+      start: formatDate(defaultStartDate),
       end: formatDate(defaultEnd)
     })    
 
