9ac7c9bea526bfa849d56873a8ccb13921c7d35f
Adnan Zaki <adnan_zaki@hotmail.co.id>
2022-06-27 15:39:27 +0700

    [SiAbsen] Add admin dashboard prototype

--- app/ui-src/src/pages/home/Index.vue
+++ app/ui-src/src/pages/home/Index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="q-pa-md">    
+  <div class="q-pa-md">   
+    <presence-flash-recap /> 
     <updates />
     <!-- <welcome /> -->
   </div>
@@ -8,12 +9,14 @@
 <script>
 import Updates from './Updates.vue'
 import Welcome from './Welcome.vue'
+import PresenceFlashRecap from 'src/siabsen_pages/dashboard/PresenceFlashRecap.vue'
 export default {
   name: 'PageIndex',
   components: {
     Welcome,
-    Updates
-  },
+    Updates,
+    PresenceFlashRecap
+},
   beforeRouteEnter(to, from) {
     if(from.fullPath === '/login') {
       window.location.reload()
---
+++ app/ui-src/src/siabsen_pages/dashboard/PresenceFlashRecap.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="q-my-md row">
+    <div :class="['col-12 col-md-4', responsiveClass()]">
+      <q-card class="my-card bg-green">
+        <q-card-section class="text-white">
+          <div class="text-h2">52</div>
+          <div class="text-subtitle1">Pegawai hadir hari ini</div>    
+          <q-btn
+            class="absolute check-btn"
+            color="green-5" unelevated size="lg"
+            icon="o_check_circle">    
+            <btn-tooltip :label="'Cek kehadiran'" anchor="center end" self="center left" />  
+          </q-btn>
+        </q-card-section>
+      </q-card>
+    </div>
+    <div :class="['col-12 col-md-4', responsiveClass()]">
+      <q-card class="my-card bg-red">
+        <q-card-section class="text-white">
+          <div class="text-h2">4</div>
+          <div class="text-subtitle1">Pegawai absen hari ini</div> 
+          <q-btn
+            class="absolute check-btn"
+            color="red-5" unelevated size="lg"
+            icon="o_cancel">    
+            <btn-tooltip :label="'Cek kehadiran'" anchor="center end" self="center left" />  
+          </q-btn>       
+        </q-card-section>
+      </q-card>
+    </div>
+    <div :class="['col-12 col-md-4', responsiveClass()]">
+      <q-card class="my-card bg-amber-8">
+        <q-card-section class="text-white">
+          <div class="text-h2">6</div>
+          <div class="text-subtitle1">Pegawai izin hari ini</div>  
+          <q-btn
+            class="absolute check-btn"
+            color="amber-6" unelevated size="lg"
+            icon="o_info">    
+            <btn-tooltip :label="'Cek permintaan izin'" anchor="center end" self="center left" />        
+          </q-btn>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+
+export default {
+  setup() {
+    const $q = useQuasar()
+
+    return {
+      responsiveClass: () => $q.screen.lt.sm ? 'q-mb-md' : 'q-pr-sm'
+    }
+  }
+}
+</script>
