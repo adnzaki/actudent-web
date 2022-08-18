1bf41de5d488ec113e426bced106eb0ab4946ed1
Adnan Zaki <adnan_zaki@hotmail.co.id>
2022-08-18 15:42:55 +0700

    return SiAbsen dashboard

--- app/ui-src/src/pages/home/Index.vue
+++ app/ui-src/src/pages/home/Index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="q-pa-md">    
+  <div class="q-pa-md">  
+    <presence-flash-recap />   
     <updates />
   </div>
 </template>
 
 <script>
+import PresenceFlashRecap from 'src/siabsen_pages/dashboard/PresenceFlashRecap.vue'
 import Updates from './Updates.vue'
 export default {
   name: 'PageIndex',
   components: {
+    PresenceFlashRecap,
     Updates
   },
   beforeRouteEnter(to, from) {
--- app/ui-src/src/pages_teacher/home/TeacherIndex.vue
+++ app/ui-src/src/pages_teacher/home/TeacherIndex.vue
@@ -7,10 +7,14 @@
 </template>
 
 <script>
+import PresenceCenter from 'src/siabsen_pages/dashboard/PresenceCenter.vue'
+import PresenceDialog from 'src/siabsen_pages/dashboard/PresenceDialog.vue'
 import Updates from 'pages/home/Updates.vue'
 export default {
   name: 'TeacherIndex',
   components: {
+    PresenceCenter,
+    PresenceDialog,
     Updates
   },
   beforeRouteEnter(to, from) {
