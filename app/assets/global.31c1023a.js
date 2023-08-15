c567ea66f6d6dd26891d74db4ac34216941e3532
Adnan Zaki <adnan_zaki@hotmail.co.id>
2023-08-15 20:25:58 +0700

    Publish build fix

---
+++ app/assets/global.31c1023a.js
@@ -0,0 +1,1 @@
+import{W as t,X as a}from"./index.31ac1f73.js";var n=({app:e})=>{e.config.globalProperties.$buildVersion="rc-2.ac.v2.0058",e.config.globalProperties.$trim=(r,o=25,i=!0)=>r===void 0?"":t(r,o,i),e.config.globalProperties.$formatDate=(r,o="dddd, DD MMMM YYYY")=>r===void 0?"":a(r,o)};export{n as default};
