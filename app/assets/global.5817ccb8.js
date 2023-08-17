1abb3ac854e661475b5a103966577ab34daaec13
Adnan Zaki <adnan_zaki@hotmail.co.id>
2023-08-17 22:46:41 +0700

    Publish update from Actudent

--- app/assets/global.5817ccb8.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:app/assets/global.5817ccb8.js
-import{T as t,U as a}from"./index.336230e6.js";var n=({app:e})=>{e.config.globalProperties.$buildVersion="rc-2.ac.v2.0057",e.config.globalProperties.$trim=(r,o=25,i=!0)=>r===void 0?"":t(r,o,i),e.config.globalProperties.$formatDate=(r,o="dddd, DD MMMM YYYY")=>r===void 0?"":a(r,o)};export{n as default};
-========
-import{W as t,X as a}from"./index.c7486aac.js";var n=({app:e})=>{e.config.globalProperties.$buildVersion="rc-3.ac.v2.0061",e.config.globalProperties.$trim=(r,o=25,i=!0)=>r===void 0?"":t(r,o,i),e.config.globalProperties.$formatDate=(r,o="dddd, DD MMMM YYYY")=>r===void 0?"":a(r,o)};export{n as default};
->>>>>>>> v2-vite:app/assets/global.c49fc810.js
