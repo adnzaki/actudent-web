9b413982d5ffb5f62fded213d075bb744757a8c2
Adnan Zaki <adnan_zaki@hotmail.co.id>
2023-09-21 09:57:35 +0700

    Publish the latest build

--- app/assets/global.a109d64a.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:app/assets/global.a109d64a.js
-import{T as i,U as a}from"./index.49ead34d.js";var l=({app:e})=>{e.config.globalProperties.$buildVersion="v2.0.0 (Stable)",e.config.globalProperties.$trim=(r,o=25,t=!0)=>r===void 0?"":i(r,o,t),e.config.globalProperties.$formatDate=(r,o="dddd, DD MMMM YYYY")=>r===void 0?"":a(r,o)};export{l as default};
-========
-import{T as i,U as a}from"./index.326f8e52.js";var l=({app:e})=>{e.config.globalProperties.$buildVersion="v2.0.0 (Stable)",e.config.globalProperties.$trim=(r,o=25,t=!0)=>r===void 0?"":i(r,o,t),e.config.globalProperties.$formatDate=(r,o="dddd, DD MMMM YYYY")=>r===void 0?"":a(r,o)};export{l as default};
->>>>>>>> v2-vite:app/assets/global.65702955.js
