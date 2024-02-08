3d6aae14df6a972feaafef035ed63656d973946d
Adnan Zaki <adnzaki@outlook.com>
2024-02-08 11:53:53 +0700

    Fix account menu text color on dark mode

--- app/ui-src/src/layouts/OtherActions.vue
+++ app/ui-src/src/layouts/OtherActions.vue
@@ -1,8 +1,11 @@
 <template>
-  <q-item 
-    clickable v-ripple class="q-pr-xl"
+  <q-item
+    clickable
+    v-ripple
+    :class="['q-pr-xl', accountMenu]"
     :to="link"
-    v-close-popup>
+    v-close-popup
+  >
     <q-item-section avatar>
       <q-icon :name="icon" />
     </q-item-section>
@@ -12,10 +15,17 @@
   </q-item>
 </template>
 <script>
+import { computed } from 'vue'
+import { headerColor } from 'src/composables/mode'
+
 export default {
   props: ['link', 'icon', 'label'],
   setup() {
-    
-  }
+    const accountMenu = computed(() => {
+      return headerColor.value === 'dark' ? 'account-menu-item-dark' : ''
+    })
+
+    return { accountMenu }
+  },
 }
 </script>
