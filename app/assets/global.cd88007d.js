4f1a4714f646411e347405c99a9180a5da9a1927
Adnan Zaki <adnzaki@outlook.com>
2024-07-23 10:24:45 +0700

    Fix bug on 2.1.0 release

--- app/assets/global.cd88007d.js
+++
@@ -1,1 +0,0 @@
-import{T as t,U as a}from"./index.d2331c77.js";var n=({app:e})=>{e.config.globalProperties.$buildVersion="2.1.0 (22-07-2024)",e.config.globalProperties.$trim=(r,o=25,i=!0)=>r===void 0?"":t(r,o,i),e.config.globalProperties.$formatDate=(r,o="dddd, DD MMMM YYYY")=>r===void 0?"":a(r,o)};export{n as default};
